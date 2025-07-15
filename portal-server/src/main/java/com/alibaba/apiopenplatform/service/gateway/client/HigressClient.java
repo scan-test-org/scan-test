96501db9803fe64e76bf981cf6d3ab7fc7fc2425
zhaoh <yanyan.zh@alibaba-inc.com>
2025-07-15 16:24:02 +0800

    fix

--- portal-server/src/main/java/com/alibaba/apiopenplatform/service/gateway/client/HigressClient.java
+++ portal-server/src/main/java/com/alibaba/apiopenplatform/service/gateway/client/HigressClient.java
@@ -3,8 +3,8 @@ package com.alibaba.apiopenplatform.service.gateway.client;
 import com.alibaba.apiopenplatform.service.gateway.factory.HTTPClientFactory;
 import com.alibaba.apiopenplatform.entity.Gateway;
 import com.alibaba.apiopenplatform.support.gateway.HigressConfig;
-import com.alibaba.higress.sdk.config.HigressServiceConfig;
-import com.alibaba.higress.sdk.service.HigressServiceProvider;
+//import com.alibaba.higress.sdk.config.HigressServiceConfig;
+//import com.alibaba.higress.sdk.service.HigressServiceProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.client.RestTemplate;
 
@@ -61,19 +61,19 @@ public class HigressClient extends GatewayClient {
         return function.apply(restTemplate);
     }
 
-    private HigressServiceProvider createProvider() {
-        HigressConfig config = instance.getHigressConfig();
-        HigressServiceConfig serviceConfig = HigressServiceConfig.builder()
-                .withControllerServiceHost(config.getHost())
-                .withControllerServicePort(config.getPort())
-                .withControllerAccessToken(config.getAccessToken())
-                .withControllerJwtPolicy(config.getJwtPolicy())
-                .build();
-
-        try {
-            return HigressServiceProvider.create(serviceConfig);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    private HigressServiceProvider createProvider() {
+//        HigressConfig config = instance.getHigressConfig();
+//        HigressServiceConfig serviceConfig = HigressServiceConfig.builder()
+//                .withControllerServiceHost(config.getHost())
+//                .withControllerServicePort(config.getPort())
+//                .withControllerAccessToken(config.getAccessToken())
+//                .withControllerJwtPolicy(config.getJwtPolicy())
+//                .build();
+//
+//        try {
+//            return HigressServiceProvider.create(serviceConfig);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
