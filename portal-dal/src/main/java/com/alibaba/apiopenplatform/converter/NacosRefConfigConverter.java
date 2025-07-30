9dd3a5482d2b90e305cb1b27c6265c7ad9949c36
zhaoh <yanyan.zh@alibaba-inc.com>
2025-07-30 17:40:49 +0800

    publication表

--- portal-dal/src/main/java/com/alibaba/apiopenplatform/converter/NacosRefConfigConverter.java
+++ portal-dal/src/main/java/com/alibaba/apiopenplatform/converter/NacosRefConfigConverter.java
@@ -1,42 +1,16 @@
 package com.alibaba.apiopenplatform.converter;
 
 import com.alibaba.apiopenplatform.support.product.NacosRefConfig;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
 /**
- * NacosRefConfig JPA 转换器
  * @author zxd
  */
 @Converter
-public class NacosRefConfigConverter implements AttributeConverter<NacosRefConfig, String> {
+public class NacosRefConfigConverter extends JsonConverter<NacosRefConfig> {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    @Override
-    public String convertToDatabaseColumn(NacosRefConfig nacosRefConfig) {
-        if (nacosRefConfig == null) {
-            return null;
-        }
-        try {
-            return objectMapper.writeValueAsString(nacosRefConfig);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to convert NacosRefConfig to JSON", e);
-        }
-    }
-
-    @Override
-    public NacosRefConfig convertToEntityAttribute(String dbData) {
-        if (dbData == null || dbData.isEmpty()) {
-            return null;
-        }
-        try {
-            return objectMapper.readValue(dbData, NacosRefConfig.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to convert JSON to NacosRefConfig", e);
-        }
+    public NacosRefConfigConverter() {
+        super(NacosRefConfig.class);
     }
 } 
