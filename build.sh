37ac00aea39d5973213f81a7a3310bfacf808b3b
LiYongjie <lyj02179318@alibaba-inc.com>
2025-07-30 10:55:46 +0800

    build(api-portal-admin): 添加构建脚本并删除冗余文件
    - 新增 build.sh 脚本，用于安装依赖、构建项目和推送镜像
    - 删除 replace_var.py、start.sh 和 proxy.conf 文件

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 设置错误时退出
+set -e
+
+# 安装依赖
+npm i
+
+# 构建
+npm run build
+
+# 构建镜像
+docker buildx build --platform linux/amd64 -t registry.cn-shanghai.aliyuncs.com/daofeng/api-portal-admin:latest .
+
+# 推送镜像
+docker push registry.cn-shanghai.aliyuncs.com/daofeng/api-portal-admin:latest
