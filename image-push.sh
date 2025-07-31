b2c868bfec6f811354962831d624f67f44fb3d28
岛风 <jingfeng.xjf@alibaba-inc.com>
2025-07-31 15:30:28 +0800

    build(Dockerfile): 重构 Dockerfile 并添加 Nginx 配置
    - 更新基础镜像为 cnstack/cnstack-alios7u2:1.0-beta
    - 添加 Nginx 配置文件和代理配置
    - 复制 dist 目录到 /app
    - 更新 CMD 为使用启动脚本
    - 移除开发依赖项

---
+++ image-push.sh
@@ -0,0 +1,14 @@
+set -e
+rm -rf ./dist
+npm run build
+
+repository=
+aliyun_user=mse_dev_chengpu_testcloud_com
+aliyun_password=
+namespace=daofeng
+image=api-portal-admin
+version=1.0.0
+
+docker buildx build -t $repository/$namespace/$image:$version --platform linux/amd64 -o type=docker .
+docker login $repository --username $aliyun_user --password $aliyun_password
+docker push $repository/$namespace/$image:$version
