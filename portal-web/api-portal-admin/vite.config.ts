06f63847fbe976a867bbb3ee3fcc61a3ffb5f32b
LiYongjie <lyj02179318@alibaba-inc.com>
2025-08-26 15:32:09 +0800

    build(api-portal-admin): 配置代理并修改开发脚本
    - 在 vite.config.ts 中添加代理配置，将 API 请求代理到本地服务器
    - 修改 package.json 中的 dev 脚本，去除 devlocal 模式

--- portal-web/api-portal-admin/vite.config.ts
+++ portal-web/api-portal-admin/vite.config.ts
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), '')
+const apiPrefix = env.VITE_API_BASE_URL
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174
+    port: 5174,
+    proxy: {
+      [apiPrefix]: {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(new RegExp(`^${apiPrefix}`), ''),
+      },
+    },
   },
   resolve: {
     alias: {
