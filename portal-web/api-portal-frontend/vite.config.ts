159f0fd05a3b4e0695f82a07a62938569c44d82c
单从 <yuefan.lyf@alibaba-inc.com>
2025-08-26 19:52:00 +0800

    chore(api-portal-frontend): 修复 npm install 和 npm run dev 问题

--- portal-web/api-portal-frontend/vite.config.ts
+++ portal-web/api-portal-frontend/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: true
   },
   optimizeDeps: {
     include: ['monaco-editor/esm/vs/editor/editor.api']
